61d59eaa7cff6d42944a44a35506a8b85a014696
mohammedmido <112119274+mohammedmido@users.noreply.github.com>
2022-12-22 03:20:16 +0200

    Add files via upload

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Prime 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first Number : ");
+            int first = int.Parse(Console.ReadLine());
+            Console.Write("Enter second Number : ");
+            int second = int.Parse(Console.ReadLine());
+            int t = 0;
+            for (int i = first; i <= second; i++)
+            {
+                for (int s = 1; s <= i; s++)
+                {
+                    if (i % s == 0)
+                    {
+                        t++;
+                    }
+                }
+                    if (t == 2)
+                    {
+                        Console.WriteLine(i);
+                        t = 0;
+
+                    }
+                    else
+                        t = 0;
+                }
+            
+
+        }
+    }
+      
+       
+    
+}
